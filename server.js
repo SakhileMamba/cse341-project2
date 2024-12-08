a0cd7270d09b4f0350cbf76a0ebbce97851c8ca4
Sakhile Mamba <sakhymamba@gmail.com>
2024-12-08 04:42:45 +0200

    cors fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 // ... other middleware and route setup ...
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
